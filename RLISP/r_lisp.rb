d7cfee6c4fdeefe56cb3f304ee0e86fc9d8a2d2d
Ryan Furrer <rfurrer@ualberta.ca>
2019-03-04 15:21:25 -0700

    RLISP class

---
+++ RLISP/r_lisp.rb
@@ -0,0 +1,25 @@
+# Ruby lisp
+class RLisp
+  def initialize()
+    # RLISP!
+  end
+
+
+  def label(L, L, L); end
+
+  def quote(L); end
+
+  def car(L); end
+
+  def cdr(L); end
+
+  def cons(L); end
+
+  def eq(L, L); end
+
+  def if(L, L, L); end
+
+  def atom(E); end
+
+  def lambda(L, L, L); end
+end
