6347b5db1a3ecbacbace7d0931092799ac06fad4
Ryan Furrer <rfurrer@ualberta.ca>
2019-03-04 15:35:05 -0700

    lowercase

---
+++ questions/r_lisp.rb
@@ -0,0 +1,25 @@
+# Ruby lisp
+class RLisp
+  def initialize()
+    # RlISP!
+  end
+
+
+  def label(l1, l2, l3); end
+
+  def quote(l); end
+
+  def car(l); end
+
+  def cdr(l); end
+
+  def cons(l); end
+
+  def eq(e1, e2); end
+
+  def if(c, s1, s2); end
+
+  def atom(e); end
+
+  def lambda(l0, l1, l2); end
+end
