4bdfc11e34fb7d13658d9812da1a4c68d3d17ac2
James Hryniw <jhryniw@gmail.com>
2019-03-04 17:31:06 -0700

    More assertions and benchmarking (#15)
    * data sanity checks
    
    * benchmark test

--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -1,9 +1,10 @@
 require 'test/unit'
+require 'benchmark'
 require_relative '../lib/concurrent_sort'
 require_relative 'utils/car'
 
 class MergeSortTest < Test::Unit::TestCase
-  TEST_ITER = 10
+  TEST_ITER = 1
   MIN_VAL = -10_000
   MAX_VAL = 10_000
   MAX_LEN = 100_000
@@ -30,56 +31,80 @@ class MergeSortTest < Test::Unit::TestCase
   def test_stream_sort; end
 
   def test_sort
-    (0..TEST_ITER).each do
+    TEST_ITER.times.each do
       arr = rand_int_array
+      arr_orig = arr.dup
 
       # Preconditions
       begin
-        arr.each {|el| el.is_a?(Comparable)}
+        arr.each {|el| assert_true(el.is_a?(Comparable))}
       end
 
       sorted_arr = ConcurrentSort.sort(arr)
 
       # Postconditions
       begin
+        assert_equal(arr_orig, arr, "Error: original array was modified")
         assert_sorted(sorted_arr) {|first, second| assert_true(first <= second, "Array not sorted: failed #{first} is not <= #{second}")}
       end
     end
   end
 
   def test_sort_block
-    (0..TEST_ITER).each do
+    TEST_ITER.times do
       arr = rand_int_array
+      arr_orig = arr.dup
 
       # Preconditions
       begin
-        arr.each {|el| el.is_a?(Comparable)}
+        arr.each {|el| assert_true(el.is_a?(Comparable))}
       end
 
       sorted_arr = ConcurrentSort.sort(arr) {|first, second| -(first <=> second)}
 
       # Postconditions
       begin
+        assert_equal(arr_orig, arr, "Error: original array was modified")
         assert_sorted(sorted_arr) {|first, second| assert_true(first >= second, "Array not sorted: failed #{first} is not >= #{second}")}
       end
     end
   end
 
   def test_sort_custom_obj
-    (0..TEST_ITER).each do
+    TEST_ITER.times do
       arr = rand_array {Car.new}
+      arr_orig = arr.dup
 
       # Preconditions
       begin
-        arr.each {|el| el.is_a?(Comparable)}
+        arr.each {|el| assert_true(el.is_a?(Comparable))}
       end
 
       sorted_arr = ConcurrentSort.sort(arr)
 
       # Postconditions
       begin
+        assert_equal(arr_orig, arr, "Error: original array was modified")
         assert_sorted(sorted_arr) {|first, second| assert_true(first.wheels <= second.wheels, "Cars not sorted: #{first.wheels} wheels is not >= #{second.wheels} wheels")}
       end
+    end
+  end
+
+  def test_benchmark
+    arr = rand_int_array(size: MAX_LEN)
+
+    reg_time = Benchmark.measure do
+      arr.sort
+    end
+
+    conc_time = Benchmark.measure do
+      ConcurrentSort.sort(arr)
+    end
+
+    if conc_time.total > reg_time.total
+      warn "Warning: Slower than regular sorting"
+      warn "Regular: #{reg_time}"
+      warn "Concurrent: #{conc_time}"
     end
   end
 end
