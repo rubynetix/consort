7ad050e0f2db10269b6e6ce0d0e16ccae433cea4
LionKingSimba <ttran1@ualberta.ca>
2019-03-04 17:54:50 -0700

    atom and eq funcs

--- questions/r_lisp.rb
+++ questions/r_lisp.rb
@@ -16,26 +16,37 @@ class RLisp
 
   def cons(*args); end
 
-  def eq(*args); end
+  def eq(args)
+    raise ArgumentError, 'Incorrect number of arguments (' + args.size().to_s + ' for 2)' if args.size() != 2
+    if args[0] == args[1]
+      true
+    else
+      false
+    end
+  end
 
   def if(*args); end
 
   def atom(arg)
-    return false if arg.is_a?
-
+    if arg.is_a? Array
+      return false
+    elsif arg.is_a? Symbol
+      return false
+    end
     true
   end
 
   def lambda(*args); end
 
   def eval(e)
+    return e if atom(e)
     func = e.shift
-    if !func.equal? :quote
+    if func.equal? :quote
+      send func, e[0]
+    else
       args = Array.new
       e.each { |item| args.append(eval(item)) }
       send func, args
-    else
-      send func, e[0]
     end
   end
 end
