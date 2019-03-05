43bf2846f33647c2767d14cc5223a170920592be
Ryan Furrer <rfurrer@ualberta.ca>
2019-03-04 21:07:01 -0700

    rlisp comds (#16)
    * Label through cons tests pass
    
    * label fix

--- questions/r_lisp.rb
+++ questions/r_lisp.rb
@@ -1,27 +1,44 @@
 # Ruby lisp
 class RLisp
+
   def initialize
-    # RlISP!
+    @labels = Hash.new
   end
 
-  def label(*args); end
+  def label(args)
+    @labels[args[0].dup] = args[1].dup
+  end
 
   def quote(arg)
+    puts arg.to_s
     arg
   end
 
-  def car(*args); end
+  def car(args)
+    raise ArgumentError unless (args.size == 1) && !atom(args[0])
 
-  def cdr(*args); end
+    args[0].shift
+  end
 
-  def cons(*args); end
+  def cdr(args)
+    raise ArgumentError unless (args.size == 1) && !atom(args[0])
+
+    args[0].shift
+    args[0]
+  end
+
+  def cons(args)
+    raise ArgumentError unless args.size == 2 && !atom(args[1])
+
+    args[1].insert(0, args[0])
+  end
 
   def eq(*args); end
 
   def if(*args); end
 
   def atom(arg)
-    return false if arg.is_a?
+    return false if arg.is_a? Array
 
     true
   end
@@ -29,13 +46,20 @@ class RLisp
   def lambda(*args); end
 
   def eval(e)
+    return @labels[e] if @labels.include? e
+    return e if atom(e)
+
     func = e.shift
-    if !func.equal? :quote
+    puts func
+    if func.equal? :quote
+      send func, e[0]
+    elsif self.class.instance_methods.to_s.include? func.to_s
       args = Array.new
       e.each { |item| args.append(eval(item)) }
-      send func, args
+      puts args.to_s
+      return send func, args
     else
-      send func, e[0]
+      e
     end
   end
 end
