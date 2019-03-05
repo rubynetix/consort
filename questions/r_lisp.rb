ed618bda24e84e1b19c6a6189f5297a1f7823894
Ryan Furrer <rfurrer@ualberta.ca>
2019-03-04 17:55:08 -0700

    Label through cons tests pass

--- questions/r_lisp.rb
+++ questions/r_lisp.rb
@@ -1,27 +1,46 @@
 # Ruby lisp
 class RLisp
   def initialize
-    # RlISP!
+    # RLISP!
+    @labels = Hash.new
   end
 
-  def label(*args); end
+  def label(*args)
+    raise ArgumentError unless args.size == 2
+
+    @labels[args[0]] = args[1]
+  end
 
   def quote(arg)
+    puts arg.to_s
     arg
   end
 
-  def car(*args); end
+  def car(args)
+    raise ArgumentError unless (args.size == 1) && !atom(args[0])
 
-  def cdr(*args); end
+    args[0].shift
+  end
 
-  def cons(*args); end
+  def cdr(args)
+    raise ArgumentError unless (args.size == 1) && !atom(args[0])
+
+    args[0].shift
+    args[0]
+  end
+
+  def cons(args)
+    raise ArgumentError unless args.size == 2 && !atom(args[1])
+
+    args[1].insert(0, args[0])
+  end
 
   def eq(*args); end
 
   def if(*args); end
 
   def atom(arg)
-    return false if arg.is_a?
+    return false if arg.is_a? Array
 
     true
   end
@@ -29,6 +48,8 @@ class RLisp
   def lambda(*args); end
 
   def eval(e)
+    return e if atom(e)
+
     func = e.shift
     if !func.equal? :quote
       args = Array.new
